ca74e23aebb4e4d456519f059b5d0f626d699997
amandajaisun <amandajaisun@gmail.com>
2023-05-12 10:34:05 -0400

    background.js now fetches cat images

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
-console.log("Cats Everywhere - Content Script is Running");
+console.log("background Cats Everywhere - Content Script is Running");
 
-self.onmessage = function(event) {
-  if (event.data.msg === 'image') {
-    fetch('https://api.thecatapi.com/v1/images/search')
-      .then(response => response.json())
-      .then(data => {
-        const response = { data: data[0], index: event.data.index };
-        event.ports[0].postMessage(response);
-      })
-      .catch(error => console.error('Error:', error));
-  }
-};
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message === "fetchData") {
+      fetch('https://api.thecatapi.com/v1/images/search')
+        .then(response => response.json())
+        .then(data => sendResponse({data: data}))
+        .catch(error => sendResponse({error: error}));
+      return true;  // Will respond asynchronously.
+    }
+  });
+
--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,29 @@
-console.log("Cats Everywhere - Content Script is Running");
+console.log("contentScript.js Cats Everywhere - Content Script is Running");
 
-let images = document.getElementsByTagName('img');
-for (let i = 0; i < images.length; i++) {
-  let messageChannel = new MessageChannel();
-  messageChannel.port1.onmessage = function (event) {
-    if (event.data.data && event.data.index != null) {
-      images[event.data.index].src = event.data.data.url;
-    }
-  };
-  if (navigator.serviceWorker.controller) {
-    navigator.serviceWorker.controller.postMessage({ msg: 'image', index: i }, [messageChannel.port2]);
+chrome.runtime.sendMessage({message: "fetchData"}, function(response) {
+  if (response.error) {
+    console.log(`Error: ${response.error}`);
   } else {
-    // Handle the case when there is no active service worker
-    console.log('No active service worker.');
+    console.log("whole response", response);
+    console.log(response.data);
+    const url = response.data[0]['url'];
+    console.log('url', url)
   }
-}
+});
+
+
+// let images = document.getElementsByTagName('img');
+// for (let i = 0; i < images.length; i++) {
+//   let messageChannel = new MessageChannel();
+//   messageChannel.port1.onmessage = function (event) {
+//     if (event.data.data && event.data.index != null) {
+//       images[event.data.index].src = event.data.data.url;
+//     }
+//   };
+//   if (navigator.serviceWorker.controller) {
+//     navigator.serviceWorker.controller.postMessage({ msg: 'image', index: i }, [messageChannel.port2]);
+//   } else {
+//     // Handle the case when there is no active service worker
+//     console.log('No active service worker.');
+//   }
+// }
