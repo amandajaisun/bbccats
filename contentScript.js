c147208803a3f7c2ddf062e4b3afa35cd4819794
amandajaisun <amandajaisun@gmail.com>
2023-05-12 11:16:36 -0400

    replaced ads with cats now working

--- contentScript.js
+++ contentScript.js
@@ -1,16 +1,46 @@
 console.log("contentScript.js Cats Everywhere - Content Script is Running");
+let elements = document.getElementsByClassName('bbccom_advert');
+// start by fetching one cat image at a time
+elements = Array.from(elements);
 
-chrome.runtime.sendMessage({message: "fetchData"}, function(response) {
-  if (response.error) {
-    console.log(`Error: ${response.error}`);
-  } else {
-    console.log("whole response", response);
-    console.log(response.data);
-    const url = response.data[0]['url'];
-    console.log('url', url)
+elements.forEach(element => {
+  let parent = element.parentElement;
+  while (parent) {
+    // Check if this is a div with a class that starts with "advert advert"
+    if (parent.tagName.toLowerCase() === 'div' &&
+      parent.className.startsWith('advert advert')) {
+      // Found it! Do whatever you need with the parent div here.
+      console.log(parent);
+
+      // And stop looking for more parents.
+      break;
+    }
+
+    // Not it. Move up to the next parent.
+    parent = parent.parentElement;
   }
-});
 
+  let img = document.createElement('img');
+
+  chrome.runtime.sendMessage({ message: "fetchData" }, function (response) {
+    if (response.error) {
+      console.log(`Error: ${response.error}`);
+    } else {
+      console.log("whole response", response);
+      console.log(response.data);
+      img.src = response.data[0]['url'];
+      console.log('url', url)
+    }
+  });
+  //img.src = "https://cdn2.thecatapi.com/images/baq.jpg";
+  img.alt = "cat";
+
+  // Set the image size
+  img.style.width = '200px';  // or any size you want
+  img.style.height = '200px'; // or any size you want
+
+  parent.replaceWith(img);
+});
 
 // let images = document.getElementsByTagName('img');
 // for (let i = 0; i < images.length; i++) {
